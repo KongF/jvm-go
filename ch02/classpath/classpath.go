412fcada240f0c9a62623021736f4a51bedd3df8
kongfq <kongfq1@belink.com>
2022-05-10 23:29:46 +0800

    ch02

--- ch02/classpath/classpath.go
+++ ch02/classpath/classpath.go
@@ -15,8 +15,20 @@ func Parse(jreOption, cpOption string) *Classpath {
 	cp.parseUserClasspath(cpOption)
 	return cp
 }
-func (self *Classpath) ReadClass(className string) ([]byte, Entry, error) {}
-func (self *Classpath) String() string                                    {}
+func (self *Classpath) ReadClass(className string) ([]byte, Entry, error) {
+	className = className + ".class"
+	if data, entry, err := self.bootClasspath.readClass(className); err == nil {
+		return data, entry, err
+	}
+	if data, entry, err := self.extClasspath.readClass(className); err == nil {
+		return data, entry, err
+	}
+	return self.userClasspath.readClass(className)
+}
+
+func (self *Classpath) String() string {
+	return self.userClasspath.String()
+}
 
 func (self *Classpath) parseBootAndExtClasspath(jreOption string) {
 	jreDir := getJreDir(jreOption)
@@ -27,6 +39,12 @@ func (self *Classpath) parseBootAndExtClasspath(jreOption string) {
 	jreExtPath := filepath.Join(jreDir, "lib", "ext", "*")
 	self.extClasspath = newWildcardEntry(jreExtPath)
 }
+func (self *Classpath) parseUserClasspath(cpOption string) {
+	if cpOption == "" {
+		cpOption = "."
+	}
+	self.userClasspath = newEntry(cpOption)
+}
 func getJreDir(jreOption string) string {
 	if jreOption != "" && exists(jreOption) {
 		return jreOption
@@ -34,11 +52,16 @@ func getJreDir(jreOption string) string {
 	if exists("./jre") {
 		return "./jre"
 	}
-
+	if jh := os.Getenv("JAVA_HOME"); jh != "" {
+		return filepath.Join(jh, "jre")
+	}
+	panic("Can not find jre folder!")
 }
 func exists(path string) bool {
 	if _, err := os.Stat(path); err != nil {
-		return false
+		if os.IsNotExist(err) {
+			return false
+		}
 	}
 	return true
 }
