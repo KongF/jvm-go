0859a87daac66c805013ec5faadf1685fe89fd50
kongfq <1028564907@qq.com>
2022-05-12 12:51:25 +0800

    读取int，long float

--- ch03/classfile/cp_numeric.go
+++ ch03/classfile/cp_numeric.go
@@ -6,7 +6,18 @@ import "math"
 CONSTANT_Integer_info/CONSTANT_Float_info {
 	u1 tag;
 	u4 bytes;
-}*/
+}
+CONSTANT_Long_info {
+	u1 tag;
+	u4 high_bytes;
+	u4 low_bytes;
+}
+CONSTANT_Double_info {
+	u1 tag;
+	u4 high_bytes;
+	u4 low_bytes;
+}
+*/
 
 type ConstantIntegerInfo struct {
 	val int32
@@ -16,6 +27,9 @@ type ConstantFloatInfo struct {
 }
 type ConstantLongInfo struct {
 	val int64
+}
+type ConstantDoubleInfo struct {
+	val float64
 }
 
 func (self *ConstantIntegerInfo) readInfo(reader *ClassReader) {
@@ -29,4 +43,8 @@ func (self *ConstantFloatInfo) readInfo(reader *ClassReader) {
 func (self *ConstantLongInfo) readInfo(reader *ClassReader) {
 	bytes := reader.readUint64()
 	self.val = int64(bytes)
+}
+func (self *ConstantDoubleInfo) readInfo(reader *ClassReader) {
+	bytes := reader.readUint64()
+	self.val = math.Float64frombits(bytes)
 }
