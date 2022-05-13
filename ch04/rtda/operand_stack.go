b170cfe48027de3bf6d9b91192ad4cc76fbeb03a
kongfq <kongfq1@belink.com>
2022-05-14 00:12:37 +0800

    ch04 day05

---
+++ ch04/rtda/operand_stack.go
@@ -0,0 +1,26 @@
+package rtda
+
+import "math"
+
+type OperandStack struct {
+	size  uint
+	slots []Slot
+}
+
+func newOperandStack(maxStack uint) *OperandStack {
+	if maxStack > 0 {
+		return &OperandStack{
+			slots: make([]Slot, maxStack),
+		}
+	}
+	return nil
+}
+
+func (self *OperandStack) PushInt(val int32) {
+	self.slots[self.size].num = val
+	self.size++
+}
+func (self *OperandStack) PopInt() int32 {
+	self.size--
+	return self.slots[self.size].num
+}
